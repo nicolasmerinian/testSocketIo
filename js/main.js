a6c36038324f1e0672d40e05be58d8fa2c4e9311
Nicolas <nicolas.merinian@gmail.com>
2017-04-01 19:05:01 +0200

    set up of basic socket

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 console.log('Hello');
+
+var http = require('http');
+var fs = require('fs');
+var idClient = 1;
+
+// Chargement du fichier index.html affiché au client
+var server = http.createServer(function(req, res) {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+
+// Chargement de socket.io
+var io = require('socket.io').listen(server);
+
+// Quand un client se connecte, on le note dans la console
+io.sockets.on('connection', function (socket) {
+    console.log('Un client est connecté ! ' + idClient);
+	idClient += 1;
+});
+
+
+server.listen(8080);
